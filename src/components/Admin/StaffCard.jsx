1330d70f31f780f16f9093912d6e0913af16c690
nik-mak <nick.makrakis@gmail.com>
2022-07-13 13:43:15 +1000

    added the add staff modal

--- src/components/Admin/StaffCard.jsx
+++ src/components/Admin/StaffCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Staff from "./Staff";
 
-const StaffCard = ({ staff }) => {
+const StaffCard = ({ staff, setStaff }) => {
   return (
     <div className="m-2 h-28 w-64 shadow-custom">
-      <Staff staff={staff} />
+      <Staff staff={staff} setStaff={setStaff}/>
     </div>
   );
 };

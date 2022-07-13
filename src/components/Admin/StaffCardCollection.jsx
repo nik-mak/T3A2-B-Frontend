b86bd6a6be1f51000dfdf694def70db7bf0313fe
nik-mak <nick.makrakis@gmail.com>
2022-07-13 13:51:15 +1000

    made staff cards larger

--- src/components/Admin/StaffCardCollection.jsx
+++ src/components/Admin/StaffCardCollection.jsx
@@ -5,9 +5,9 @@ function StaffCardCollection({ staff, setStaff }) {
   return (
     <>
       <h2 className="m-2 font-oswald text-4xl">Staff</h2>
-      <section className="grid grid-flow-row auto-rows-max grid-cols-1 content-evenly justify-items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <section className="grid grid-flow-row auto-rows-max grid-cols-1 content-evenly justify-items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {staff.map((staff) => (
-          <StaffCard staff={staff} setStaff={setStaff} />
+          <StaffCard key={staff._id} staff={staff} setStaff={setStaff} />
         ))}
       </section>
     </>

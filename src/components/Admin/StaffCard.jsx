7f5c0b31d2495e1bd5cd3a265da1fca659799fbc
nik-mak <nick.makrakis@gmail.com>
2022-07-13 13:51:15 +1000

    made staff cards larger

--- src/components/Admin/StaffCard.jsx
+++ src/components/Admin/StaffCard.jsx
@@ -3,7 +3,7 @@ import Staff from "./Staff";
 
 const StaffCard = ({ staff, setStaff }) => {
   return (
-    <div className="m-2 h-28 w-64 shadow-custom">
+    <div className="m-2 h-28 w-80 shadow-custom">
       <Staff staff={staff} setStaff={setStaff}/>
     </div>
   );

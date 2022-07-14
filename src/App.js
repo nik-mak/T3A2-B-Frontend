fa0bd22528db8a9e08242e716b546cf07f1635a6
nik-mak <nick.makrakis@gmail.com>
2022-07-15 09:41:16 +1000

    fixed conflicts

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Alerts from "./components/Alerts";
 import GlobalContexts from "./components/GlobalContexts";
 import Catalogue from "./pages/Catalogue";
-<<<<<<< HEAD
 import Cart from "./components/Cart/Cart";
-=======
 import Bag from "./pages/Bag";
 import AdminDashboard from "./pages/AdminDashboard";
->>>>>>> a0f893259cd609bd14827eaa152d56ed8ab5b58a
 
 // import Catalogue from "./pages/Catalogue";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-<<<<<<< HEAD
         'ghost-white': '#F9F9F9',
         'cyan-cobalt-blue': '#2555AD',
         'ue-red': '#B30001',
         'philippine-yellow ': '#FAC904',
         'checkout-blue': '#2B62C2'
-=======
-        "ghost-white": "#F9F9F9",
-        "cyan-cobalt-blue": "#2555AD",
-        "ue-red": "#B30001",
-        "philippine-yellow ": "#FAC904",
->>>>>>> a0f893259cd609bd14827eaa152d56ed8ab5b58a
       },
       fontFamily: {
         oswald: ["Oswald"],
@@ -24,13 +17,8 @@ module.exports = {
         tall: { raw: "(max-height:700px)" },
       },
       boxShadow: {
-<<<<<<< HEAD
         'custom': '2px 2px 2px 2px rgba(0, 0, 0, .1)',
         'custom2': '0px 4px 4px rgba(0, 0, 0, .25)'
-=======
-        card: "5px 10px 10px 5px rgba(0, 0, 0, 0.3)",
-        custom: "2px 2px 2px 2px rgba(0, 0, 0, .1)",
->>>>>>> a0f893259cd609bd14827eaa152d56ed8ab5b58a
       },
     },
   },

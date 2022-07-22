88cd4f8638e3f60639adbfe771fe17d7855ccbb6
nik-mak <nick.makrakis@gmail.com>
2022-07-22 14:49:46 +1000

    updated installation instruction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./index.css";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Alerts from "./components/Alerts";
 import GlobalContexts from "./components/GlobalContexts";
 import Catalogue from "./pages/Catalogue";
 import Bag from "./pages/Bag";
 import AdminDashboard from "./pages/AdminDashboard";
-import PrivateRoute from "./components/auth/PrivateRoute";
+import PrivateRoute from "./components/Auth/PrivateRoute";
 import ActiveModals from "./components/modals/ActiveModals";
 import PageNotFound from "./pages/PageNotFound";
 // import Confirmation from "./components/Confirmation";
